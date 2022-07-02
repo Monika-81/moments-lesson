44e9972f198a68f28a45e871f33c1698026e8468
Monika Jakobsson <nimrin81@gmail.com>
2022-07-02 21:56:29 +0000

    Add current user context

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Navbar, Nav, Container } from 'react-bootstrap'
 import logo from '../assets/logo.png'
 import { NavLink} from 'react-router-dom'
 import styles from '../styles/NavBar.module.css'
+import { CurrentUserContext } from '../App'
 
 const NavBar = () => {
+    const currentUser = useContext(CurrentUserContext)
+    const loggedInIcons = <>{currentUser?.username}</>
+    const loggedOutIcons = 
+        <> 
+            <NavLink 
+                className={styles.NavLink} 
+                activeClassName={styles.Active} to="/signin">
+                    <i className='fas fa-sign-in-alt'></i>Sign In!
+            </NavLink>
+            <NavLink 
+                className={styles.NavLink} 
+                activeClassName={styles.Active} to="/signup">
+                    <i className='fas fa-user-plus'></i>Sign Up!
+            </NavLink>
+        </>
+
     return (
         <Navbar className={styles.NavBar} expand="md" fixed="top">
             <Container>
@@ -21,16 +38,7 @@ const NavBar = () => {
                             activeClassName={styles.Active} to="/">
                                 <i className='fas fa-home'></i>Home
                         </NavLink>
-                        <NavLink 
-                            className={styles.NavLink} 
-                            activeClassName={styles.Active} to="/signin">
-                                <i className='fas fa-sign-in-alt'></i>Sign In!
-                        </NavLink>
-                        <NavLink 
-                            className={styles.NavLink} 
-                            activeClassName={styles.Active} to="/signup">
-                                <i className='fas fa-user-plus'></i>Sign Up!
-                        </NavLink>
+                        {currentUser ? loggedInIcons : loggedOutIcons}
                     </Nav>
                 </Navbar.Collapse>
             </Container>
