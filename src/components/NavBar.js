b5298be4cf2901bf48045bb7d6778851a16154b4
Monika Jakobsson <nimrin81@gmail.com>
2022-07-02 23:51:10 +0000

    add function to burger icon with custom hook

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,11 +6,14 @@ import styles from '../styles/NavBar.module.css'
 import { useCurrentUser, useSetCurrentUser } from '../contexts/CurrentUserContext'
 import Avatar from "./Avatar";
 import axios from "axios";
+import useClickOutsideToggle from '../hooks/useClickOutsideToggle'
 
 
 const NavBar = () => {
     const currentUser = useCurrentUser();
     const setCurrentUser = useSetCurrentUser();
+
+    const {expanded, setExpanded, ref} = useClickOutsideToggle();
 
     const handleSignOut = async () => {
         try {
@@ -54,7 +57,7 @@ const NavBar = () => {
                 className={styles.NavLink}
                 to={`/profiles/${currentUser?.profile_id}`}>
                 <Avatar src={currentUser?.profile_image} text="Profile" height={40} />
-      </NavLink>
+            </NavLink>
         </>
     )
     
@@ -74,7 +77,7 @@ const NavBar = () => {
     )
 
     return (
-        <Navbar className={styles.NavBar} expand="md" fixed="top">
+        <Navbar expanded={expanded} className={styles.NavBar} expand="md" fixed="top">
             <Container>
                 <NavLink to="/">
                     <Navbar.Brand>
@@ -82,7 +85,10 @@ const NavBar = () => {
                     </Navbar.Brand>
                 </NavLink>
                 {currentUser && addPostIcon}
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Toggle 
+                    ref={ref}
+                    onClick={() => setExpanded(!expanded)} 
+                    aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto text-left">
                         <NavLink exact 
